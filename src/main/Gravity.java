00d3532a1d3e66f87dea1aa25fc370845fd544de
Sonu <sonu.pandey_cs18@gla.ac.in>
2019-08-21 00:39:50 +0530

    add object:scanner for class:Scanner

--- src/main/Gravity.java
+++ src/main/Gravity.java
@@ -7,9 +7,12 @@ package main;
 // import java.util.Scanner;
 // uncomment the above line to use input in your program.
 
+import java.util.Scanner;
+
 public class Gravity {
 	// TODO 1: Create the main method
 	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 		/* TODO 2:
 		 *   a. Declare all your necessary variables inside main method.
 		 *   b. Declare a constant for 'gravity', assume the value to be 9.8
