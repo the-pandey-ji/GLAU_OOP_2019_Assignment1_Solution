f00ba22e2127600235c353b4fde537a09b18030a
Sonu <sonu.pandey_cs18@gla.ac.in>
2019-08-21 00:59:26 +0530

    add output statement

--- src/main/Gravity.java
+++ src/main/Gravity.java
@@ -14,6 +14,8 @@ public class Gravity {
 	public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         float time = scanner.nextFloat();
+		float g = 9.8f;
+		System.out.println("The speed of the object at " + time + " seconds after its release is " + g * time + " and the distance the object has travelled in the " + time + " seconds after the relase is " + .5 * g * time * time);
 		/* TODO 2:
 		 *   a. Declare all your necessary variables inside main method.
 		 *   b. Declare a constant for 'gravity', assume the value to be 9.8
